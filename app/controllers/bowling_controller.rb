463a6126d686cc5db0b6ebae0d3597525b0705cc
Phil DeLong <philjdelong@gmail.com>
2022-07-11 16:11:40 -0600

    Initial commit

---
+++ app/controllers/bowling_controller.rb
@@ -0,0 +1,5 @@
+class BowlingController < ApplicationController
+    def index
+        @game_score = 1
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+	get "/", to: "bowling#index"
+end
