ab5fdfe5d887959c83d80dc6e3eb1cc33ccb0dc3
Phil DeLong <philjdelong@gmail.com>
2022-07-11 19:15:50 -0600

    Create page for bowling simulator

--- app/controllers/bowling_controller.rb
+++ app/controllers/bowling_controller.rb
@@ -1,5 +1,9 @@
 class BowlingController < ApplicationController
     def index
-        @game_score = 1
+        game = Game.new()
+        @game_quote = "SHUT THE FUCK UP DONNIE"
+        @game_score = 0
+        @rolls = game.generate_rolls
+        @game_score = game.calculate_game_score(@rolls)
     end
 end
